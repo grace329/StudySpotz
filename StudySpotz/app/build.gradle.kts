43aa1639e4d809c5c4c65f568d68febee1dfe143
Julianne Jorda <julianne.jorda@yahoo.com>
2024-10-02 13:01:24 -0400

    Connecting database

--- StudySpotz/app/build.gradle.kts
+++ StudySpotz/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -37,6 +38,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.firebase.analytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
